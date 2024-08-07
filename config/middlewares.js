6304044bfa8b75a2428fd23172e6cdb02b757c26
JWPapi <juliantosun@gmail.com>
2024-08-07 09:02:35 +0100

    'deploy'

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,20 +8,8 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'dl.airtable.com',
-            'strapi-upload-us-east-1.s3.eu-east-1.amazonaws.com',
-          ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'dl.airtable.com',
-            'strapi-upload-us-east-1.s3.eu-east-1.amazonaws.com',
-          ],
+          'img-src': ["'self'", 'data:', 'blob:', '*.amazonaws.com'],
+          'media-src': ["'self'", 'data:', 'blob:'],
           upgradeInsecureRequests: null,
         },
       },
